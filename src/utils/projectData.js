029db00bbd7c30cbae1fe7dd661b619943d511fc
Jamie Carter <jc2820@hotmail.com>
2020-02-06 09:01:28 +0000

    add data file to hold general info

---
+++ src/utils/projectData.js
@@ -0,0 +1,40 @@
+const projects = [
+    {
+        name: "",
+        description: "",
+        stack: "",
+        url: "https://github.com/jc2820/" 
+    },
+    {
+        name: "",
+        description: "",
+        stack: "",
+        url: "https://github.com/jc2820/" 
+    },
+    {
+        name: "",
+        description: "",
+        stack: "",
+        url: "https://github.com/jc2820/" 
+    },
+    {
+        name: "",
+        description: "",
+        stack: "",
+        url: "https://github.com/jc2820/" 
+    },
+    {
+        name: "",
+        description: "",
+        stack: "",
+        url: "https://github.com/jc2820/" 
+    },
+    {
+        name: "",
+        description: "",
+        stack: "",
+        url: "https://github.com/jc2820/" 
+    }
+]
+
+export { projects };
