3d78e1379bc12182013a0a7710f6c783cd1ada10
Jamie Carter <jc2820@hotmail.com>
2020-02-06 23:11:20 +0000

    Add extra details to projectData

--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -6,55 +6,71 @@ import rrr from "../assets/rrr.png";
 import sortingHat from "../assets/sortinghat.png";
 import wtf from "../assets/wtf.png";
 import placeHolder from "../assets/placeholder.png";
-import calculator from "../assets/calculator.png"
+import calculator from "../assets/calculator.png";
 
 const projectData = [
   {
     name: "Gateway 2 Heritage",
-    description: "An image licensing platform for small heritage museums in the UK.",
-    stack: "ReactJS, React Router, Styled Components, Netlify Functions, Airtable, Travis, Codecov, Jest",
+    description:
+      "An image licensing platform for small heritage museums in the UK.",
+    stack:
+      "ReactJS, React-Router, Styled-Components, Netlify Lambda Functions, Airtable, TravisCI, Codecov, Jest",
+    url: "",
     gitUrl: "https://github.com/fac18/t4b-g2h",
     imgSrc: placeHolder
   },
   {
     name: "Reduce, Reuse, Recycle!",
-    description: "A game app designed to teach kids about recycling and the environment.",
-    stack: "ReactJS, React Router, Styled Components, Travis CI, Codecov, Jest",
+    description:
+      "A game app designed to teach kids about recycling and the environment.",
+    stack:
+      "ReactJS, React-Router, Styled-Components, PWA, TravisCI, Codecov, Jest, Netlify",
+    url: "https://reduce-reuse-recycle.netlify.com",
     gitUrl: "https://github.com/fac18/recycling-game",
     imgSrc: rrr
   },
   {
     name: "The Game of Life",
-    description: "A reworked version of the original 1860 'Game of Life' board game.",
-    stack: "ReactJS, CSS, Travis CI, Jest, Netlify",
+    description:
+      "A reworked version of the original 1860 'Game of Life' board game.",
+    stack: "ReactJS, CSS, TravisCI, Jest, Netlify",
+    url: "https://the-game-of-life-1860.netlify.com/",
     gitUrl: "https://github.com/fac18/reactweek-jb",
     imgSrc: gameOfLife
   },
   {
     name: "Prepper",
     description: "An apocolypse prepping hub with tips and advice.",
-    stack: "ExpressJS, Handlebars, PostgreSQL, CSS, Travis CI, Tape, Supertest, Heroku",
+    stack:
+      "ExpressJS, Handlebars, PostgreSQL, CSS, TravisCI, Tape, Supertest, Heroku",
+    url: "https://jamarad.herokuapp.com/",
     gitUrl: "https://github.com/fac18/week8-denk",
     imgSrc: prepper
   },
   {
     name: "Sorting Hat",
-    description: "A Harry Potter Sorting Hat app with database and authentication.",
-    stack: "HTML, CSS, JavaScript, NodeJS, BCrypt, JSONWebToken, Travis CI, Tape, Supertest, Codecov, Heroku",
+    description:
+      "A Harry Potter Sorting Hat app with database and authentication.",
+    stack:
+      "HTML, CSS, JavaScript, NodeJS, PostgreSQL, BCrypt, JSONWebToken, TravisCI, Tape, Supertest, Codecov, Heroku",
+    url: "https://kgbj-week-7.herokuapp.com/",
     gitUrl: "https://github.com/fac18/kgbj-week-7",
     imgSrc: sortingHat
   },
   {
     name: "CryptoWatch",
     description: "A cryptocurrency converter powered by Coin Gecko API.",
-    stack: "HTML, CSS, JavaScript, NodeJS, Coin Gecko API, Tape, Supertest, Codecov, Heroku",
+    stack:
+      "HTML, CSS, JavaScript, NodeJS, Coin Gecko API, Tape, Supertest, TravisCI, Codecov, Heroku",
+    url: "https://cryptowatch.herokuapp.com/",
     gitUrl: "https://github.com/fac18/week5-famk-backend-api",
     imgSrc: cryptoWatch
   },
   {
     name: "WTF: Where's the Food?",
     description: "Search for a country to reveal their national dish.",
-    stack: "HTML, CSS, JavaScript, NodeJS, Tape, NYC",
+    stack: "HTML, CSS, JavaScript, NodeJS, Tape, NYC, TravisCI, Heroku",
+    url: "https://wtf-wheres-the-food.herokuapp.com/",
     gitUrl: "https://github.com/fac18/week4-cikp-autocomplete",
     imgSrc: wtf
   },
@@ -62,13 +78,16 @@ const projectData = [
     name: "Gifsthesia",
     description: "Guess the song title by analysing the gifs.",
     stack: "HTML, CSS, JavaScript, Tape, Giphy API, Musicmatch API",
+    url: "https://fac18.github.io/week3-cikp-gifsthesia/",
     gitUrl: "https://github.com/jc2820/",
     imgSrc: gifsthesia
   },
   {
     name: "Calculator",
-    description: "A vanilla JavaScript calculator made as pre-course work for Founders and Coders",
+    description:
+      "A vanilla JavaScript calculator made as pre-course work for Founders and Coders",
     stack: "HTML, CSS, JavaScript, Git",
+    url: "https://jc2820.github.io/calculator/",
     gitUrl: "https://github.com/jc2820/calculator",
     imgSrc: calculator
   }
